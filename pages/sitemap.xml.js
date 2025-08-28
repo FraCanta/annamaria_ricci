ec5983bd645070070aa29bdc840adefde444eb6b
FraCanta <fcantale14@gmail.com>
2025-08-28 16:30:22 +0200

    sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -4,54 +4,80 @@ import { GET_POSTS_FOR_SITEMAP } from "@/utils/queries";
 
 const siteUrl = "https://annamariaricci.eu";
 
-// Pagine statiche
-const staticPages = [
-  "/",
-  "/chi-sono",
-  "/i-miei-strumenti",
-  "/tutti-i-percorsi",
-  "/respiro-circolare-consapevole",
-  "/blog",
-  "/contatti",
-];
-
 function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${staticPages
-    .map(
-      (path) => `
-    <url>
-      <loc>${siteUrl}${path}</loc>
-      <changefreq>weekly</changefreq>
-      <priority>1.0</priority>
-    </url>`
-    )
-    .join("")}
+
+  <url>
+    <loc>${siteUrl}/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+  <url>
+    <loc>${siteUrl}/chi-sono</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+  <url>
+    <loc>${siteUrl}/i-miei-strumenti</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+  <url>
+    <loc>${siteUrl}/tutti-i-percorsi</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+  <url>
+    <loc>${siteUrl}/respiro-circolare-consapevole</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+  <url>
+    <loc>${siteUrl}/blog</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+  <url>
+    <loc>${siteUrl}/contatti</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
 
   ${posts
     .map(({ node }) => {
       const lastMod = new Date(node.modified || node.date).toISOString();
       return `
-    <url>
-      <loc>${siteUrl}${node.uri}</loc>
-      <lastmod>${lastMod}</lastmod>
-      <changefreq>weekly</changefreq>
-      <priority>0.8</priority>
-    </url>`;
+  <url>
+    <loc>${siteUrl}${node.uri}</loc>
+    <lastmod>${lastMod}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`;
     })
     .join("")}
+
 </urlset>`;
 }
 
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
 export async function getServerSideProps({ res }) {
-  // Recupera i post da WP
   const data = await client.request(GET_POSTS_FOR_SITEMAP);
   const posts = data?.posts?.edges || [];
 
   const sitemap = generateSiteMap(posts);
 
-  res.setHeader("Content-Type", "application/xml"); // 👈 più sicuro di text/xml
+  res.setHeader("Content-Type", "application/xml");
+  res.setHeader("Cache-Control", "public, max-age=0, must-revalidate");
   res.write(sitemap);
   res.end();
 
@@ -60,6 +86,4 @@ export async function getServerSideProps({ res }) {
   };
 }
 
-export default function SiteMap() {
-  return null;
-}
+export default SiteMap;
