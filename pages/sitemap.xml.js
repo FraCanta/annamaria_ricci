1d79c3569d16d14e788fbebf2ff27be20a3235ae
FraCanta <fcantale14@gmail.com>
2025-09-01 14:06:02 +0200

    sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,10 +1,7 @@
 // pages/sitemap.xml.js
-import { client } from "@/utils/graphql";
-import { GET_POSTS_FOR_SITEMAP } from "@/utils/queries";
+const siteUrl = "https://www.annamariaricci.eu";
 
-const siteUrl = "https://annamariaricci.eu";
-
-function generateSiteMap(posts) {
+function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
@@ -50,31 +47,15 @@ function generateSiteMap(posts) {
     <priority>1.0</priority>
   </url>
 
-  ${posts
-    .map(({ node }) => {
-      const lastMod = new Date(node.modified || node.date).toISOString();
-      return `
-  <url>
-    <loc>${siteUrl}${node.uri}</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>`;
-    })
-    .join("")}
-
 </urlset>`;
 }
 
 function SiteMap() {
-  // getServerSideProps will do the heavy lifting
+  // getServerSideProps gestisce tutto
 }
 
 export async function getServerSideProps({ res }) {
-  const data = await client.request(GET_POSTS_FOR_SITEMAP);
-  const posts = data?.posts?.edges || [];
-
-  const sitemap = generateSiteMap(posts);
+  const sitemap = generateSiteMap();
 
   res.setHeader("Content-Type", "text/xml");
   res.setHeader("Cache-Control", "public, max-age=0, must-revalidate");
