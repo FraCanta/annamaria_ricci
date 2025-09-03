7f5e18642a3345737bd5f19479ca2018afbb8693
FraCanta <fcantale14@gmail.com>
2025-09-03 17:08:02 +0200

    sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -9,65 +9,6 @@ function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-  <url>
-    <loc>${siteUrl}/</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-
-  <url>
-    <loc>${siteUrl}/chi-sono</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-
-  <url>
-    <loc>${siteUrl}/i-miei-strumenti</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-
-  <url>
-    <loc>${siteUrl}/tutti-i-percorsi</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-
-  <url>
-    <loc>${siteUrl}/tutti-i-percorsi/privati/trova-la-tua-direzione</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-
-  <url>
-    <loc>${siteUrl}/tutti-i-percorsi/privati/cambia-e-trova-la-tua-strada-nel-lavoro</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-
-  <url>
-    <loc>${siteUrl}/tutti-i-percorsi/privati/trova-il-lavoro-che-desideri</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-
-  <url>
-    <loc>${siteUrl}/tutti-i-percorsi/privati/trova-le-tue-radici</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-
-  <url>
-    <loc>${siteUrl}/blog</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-
-  <url>
-    <loc>${siteUrl}/contatti</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
 
   ${posts
     .map(({ node }) => {
