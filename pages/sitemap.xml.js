b4965f6c448099e207bef00a1071ac9eaf4e3afa
FraCanta <fcantale14@gmail.com>
2025-09-03 14:28:37 +0200

    sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -5,104 +5,108 @@ import { GET_POSTS_FOR_SITEMAP } from "@/utils/queries";
 const siteUrl = "https://www.annamariaricci.eu";
 
 function generateSiteMap(posts) {
-  // Rimuoviamo duplicati e codifichiamo URI
-  const postUrls = posts
-    .map(({ node }) => ({
-      loc: `${siteUrl}/posts${encodeURI(node.uri)}`,
-      lastmod: new Date(node.date).toISOString(),
-    }))
-    .filter((v, i, a) => a.findIndex((x) => x.loc === v.loc) === i); // elimina duplicati
-
+  console.log(posts);
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
   <url>
-    <loc>${siteUrl}/</loc>
+    <loc>https://www.annamariaricci.eu/</loc>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
 
   <url>
-    <loc>${siteUrl}/chi-sono</loc>
+    <loc>https://www.annamariaricci.eu/chi-sono</loc>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
 
   <url>
-    <loc>${siteUrl}/i-miei-strumenti</loc>
+    <loc>https://www.annamariaricci.eu/i-miei-strumenti</loc>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
 
   <url>
-    <loc>${siteUrl}/tutti-i-percorsi</loc>
+    <loc>https://www.annamariaricci.eu/tutti-i-percorsi</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.annamariaricci.eu/tutti-i-percorsi/privati/trova-la-tua-direzione</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+    <url>
+    <loc>https://www.annamariaricci.eu/tutti-i-percorsi/privati/cambia-e-trova-la-tua-strada-nel-lavoro</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+   <url>
+    <loc>https://www.annamariaricci.eu/tutti-i-percorsi/privati/cambia-e-trova-la-tua-strada-nel-lavoro</loc>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
 
   <url>
-  <loc>${siteUrl}/tutti-i-percorsi/privati/trova-la-tua-direzione</loc>
-  <changefreq>weekly</changefreq>
-  <priority>1.0</priority>
-</url>
-
-<url>
-  <loc>${siteUrl}/tutti-i-percorsi/privati/cambia-e-trova-la-tua-strada-nel-lavoro</loc>
-  <changefreq>weekly</changefreq>
-  <priority>1.0</priority>
-</url>
-
-<url>
-  <loc>${siteUrl}/tutti-i-percorsi/privati/trova-il-lavoro-che-desideri</loc>
-  <changefreq>weekly</changefreq>
-  <priority>1.0</priority>
-</url>
-
-<url>
-  <loc>${siteUrl}/tutti-i-percorsi/privati/trova-le-tue-radici</loc>
-  <changefreq>weekly</changefreq>
-  <priority>1.0</priority>
-</url>
-
-
+    <loc>https://www.annamariaricci.eu/tutti-i-percorsi/privati/trova-il-lavoro-che-desideri</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+   <url>
+    <loc>https://www.annamariaricci.eu/tutti-i-percorsi/privati/trova-le-tue-radici</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
   <url>
-    <loc>${siteUrl}/blog</loc>
+    <loc>https://www.annamariaricci.eu/tutti-i-percorsi/privati/trova-le-tue-radici</loc>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
 
   <url>
-    <loc>${siteUrl}/contatti</loc>
+    <loc>https://www.annamariaricci.eu/blog</loc>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
 
-  ${postUrls
-    .map(
-      ({ loc, lastmod }) => `
   <url>
-    <loc>${loc}</loc>
-    <lastmod>${lastmod}</lastmod>
+    <loc>https://www.annamariaricci.eu/contatti</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+  ${posts
+    .map(({ node }) => {
+      const lastMod = new Date(node.date);
+      const isoDate = lastMod.toISOString();
+      return `
+  <url>
+    <loc>${siteUrl}/posts${node.uri}</loc>
+              <lastmod>${`${isoDate}`}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.5</priority>
-  </url>`
-    )
+  </url>`;
+    })
     .join("")}
 
 </urlset>`;
 }
 
 function SiteMap() {
-  // getServerSideProps gestisce tutto
+  // getServerSideProps will do the heavy lifting
 }
 
 export async function getServerSideProps({ res }) {
   const data = await client.request(GET_POSTS_FOR_SITEMAP);
+
   const posts = data?.posts?.edges || [];
 
   const sitemap = generateSiteMap(posts);
 
-  res.setHeader("Content-Type", "application/xml"); // ⚡ modificato
+  res.setHeader("Content-Type", "text/xml");
+  // we send the XML to the browser
   res.write(sitemap);
   res.end();
 
