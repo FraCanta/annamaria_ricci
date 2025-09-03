afda0eed8c7623c67f2baa5009658ea719db5a31
FraCanta <fcantale14@gmail.com>
2025-09-03 16:22:53 +0200

    sitemap

--- pages/sitemap2.xml.js
+++ pages/sitemap2.xml.js
@@ -1,6 +1,6 @@
 const siteUrl = "https://annamariaricci.eu";
 
-function generateSiteMap(posts) {
+function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
@@ -64,13 +64,19 @@ function generateSiteMap(posts) {
     <priority>1.0</priority>
   </url>
 
- 
-
 </urlset>`;
 }
 
-function SiteMap() {
-  // getServerSideProps gestisce tutto
+export async function getServerSideProps({ res }) {
+  const sitemap = generateSiteMap();
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return { props: {} };
 }
 
-export default SiteMap;
+export default function SiteMap() {
+  return null;
+}
